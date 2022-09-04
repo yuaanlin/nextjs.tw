97bb409f90ce7567134e7748a4627f901d7c904b
YONG-LIN-LIANG <steven841221@gmail.com>
2022-09-04 13:35:19 +0800

    fix: 按鈕組件拿掉Link

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,4 @@
 import { PropsWithChildren } from 'react';
-import Link from 'next/link';
 export default ({children, content, url}: PropsWithChildren<{content: string, url: string}>) => (
   <button>
     <a href={url} className="inline-block px-8 py-2 mt-8 text-white text-center bg-[#0070f3] rounded-lg cursor-pointer shadow-[rgba(0,118,255,.39)] shadow-lg hover:opacity-90">
