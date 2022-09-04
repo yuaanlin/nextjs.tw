ccc29a7910c887c0ce02fc0a02ee63c5f585a4d3
YONG-LIN-LIANG <steven841221@gmail.com>
2022-09-04 13:57:22 +0800

    fix: eslint format 調整

--- components/Blockquote.tsx
+++ components/Blockquote.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from 'react';
-export default ({children}: PropsWithChildren<{}>) => (
-  <div className='px-4 py-1 my-8 bg-[#f7f7f7]'>
+
+export default ({ children }: PropsWithChildren<{}>) => (
+  <div className="px-4 py-1 my-8 bg-[#f7f7f7]">
     {children}
   </div>
-) 
+);
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,16 @@
 import { PropsWithChildren } from 'react';
-export default ({content, url}: PropsWithChildren<{content: string, url: string}>) => (
+
+export default (
+  { content, url }: PropsWithChildren<{content: string, url: string}>
+) => (
   <button>
-    <a href={url} className="inline-block px-8 py-2 mt-8 text-white text-center bg-[#0070f3] rounded-lg cursor-pointer shadow-[rgba(0,118,255,.39)] shadow-lg hover:opacity-90">
+    <a
+      href={url}
+      className="inline-block px-8 py-2 mt-8 text-white text-center
+      bg-[#0070f3] rounded-lg cursor-pointer shadow-[rgba(0,118,255,.39)]
+      shadow-lg hover:opacity-90"
+    >
       {content}
     </a>
   </button>
-)
+);
