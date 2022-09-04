5d004fecb8d26c23b9d3a3c8fdc99bb3d4a3e81b
YONG-LIN-LIANG <steven841221@gmail.com>
2022-09-04 13:33:51 +0800

    fix: 調整按鈕組件

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,10 @@
 import { PropsWithChildren } from 'react';
-export default ({children}: PropsWithChildren<{}>) => (
-  <button className="inline-block px-8 mt-8 text-white text-center bg-[#0070f3] rounded-lg cursor-pointer shadow-[rgba(0,118,255,.39)] shadow-lg hover:opacity-90">
+import Link from 'next/link';
+export default ({children, content, url}: PropsWithChildren<{content: string, url: string}>) => (
+  <button>
+    <a href={url} className="inline-block px-8 py-2 mt-8 text-white text-center bg-[#0070f3] rounded-lg cursor-pointer shadow-[rgba(0,118,255,.39)] shadow-lg hover:opacity-90">
+      {content}
+    </a>
     {children}
   </button>
 )
