446980a02c17cb0f991ac368fbd9749b733cafc9
YONG-LIN-LIANG <steven841221@gmail.com>
2022-09-04 13:34:41 +0800

    fix: 調整按鈕組件

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,6 +5,5 @@ export default ({children, content, url}: PropsWithChildren<{content: string, ur
     <a href={url} className="inline-block px-8 py-2 mt-8 text-white text-center bg-[#0070f3] rounded-lg cursor-pointer shadow-[rgba(0,118,255,.39)] shadow-lg hover:opacity-90">
       {content}
     </a>
-    {children}
   </button>
 )
