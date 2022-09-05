4318e60d3fe08b7ca918693cde42fe409da2af24
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-09-05 12:03:27 +0800

    improve styles

--- components/Layout/DocsLayout.tsx
+++ components/Layout/DocsLayout.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/router';
 
 function DocsLayout(props: PropsWithChildren<{}>) {
   return(<>
-    <div className="mx-auto container docs mt-8 pb-32 z-20 flex">
+    <div className="mx-auto container mt-8 pb-32 z-20 flex">
       <aside
         className="z-10 h-screen sticky hidden
          md:flex md:flex-col md:w-[300px] md:min-w-[300px] md:mr-4 md:pr-5"
@@ -14,7 +14,7 @@ function DocsLayout(props: PropsWithChildren<{}>) {
         <SearchInput className="my-6"/>
         <DocsMenu />
       </aside>
-      <div className="w-full overflow-hidden">
+      <div className="w-full overflow-hidden docs">
         {props.children}
       </div>
     </div>
