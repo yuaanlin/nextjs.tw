bcb04a4f40cb4b54e88039c8dd40505f2f3e76f0
Chia1104 <yuyuchia7423@gmail.com>
2022-10-04 10:00:27 +0800

    feat: Add animation.

--- components/Layout/MiniMenu.tsx
+++ components/Layout/MiniMenu.tsx
@@ -11,16 +11,22 @@ const MiniMenu: FC<{
   return (
     <div className={`w-full flex flex-col bg-white ${className}`}>
       <button
-        className="w-full border-b"
+        className="w-full border-b focus:no-underline focus:outline-none"
         onClick={() => setIsOpen(!isOpen)}>
-        <span className="w-full h-[56px] flex gap-5 items-center mx-auto container">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <span
+          className="w-full h-[56px] flex gap-5 items-center mx-auto container">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={`w-6 h-6 transition duration-300
+            ${ isOpen && 'rotate-[90deg]' }`}>
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d={isOpen ?
-                'M19.5 8.25l-7.5 7.5-7.5-7.5' :
-                'M8.25 4.5l7.5 7.5-7.5 7.5'} />
+              d="M8.25 4.5l7.5 7.5-7.5 7.5" />
           </svg>
           <p>
             {title}
@@ -29,9 +35,11 @@ const MiniMenu: FC<{
       </button>
       <div
         className={`w-full overflow-y-scroll flex flex-col 
-        bg-white shadow-lg rounded-b-lg
-        ${isOpen ? 'block' : 'hidden'}`}>
-        <div className="`w-full h-[500px] mx-auto container" onClick={() => setIsOpen(false)}>
+        bg-white shadow-lg rounded-b-lg`}>
+        <div
+          className={`w-full mx-auto container transition-all duration-300
+          ${ isOpen ? 'h-[500px]' : 'h-0' }`}
+          onClick={() => setIsOpen(false)}>
           {children}
         </div>
       </div>
